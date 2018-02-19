174f8e7e4cac66a5ed2eb0ef160ca6604c6bcafe
Brandon <brandonreallen@gmail.com>
2018-02-18 20:21:29 -0500

    First commit

---
+++ source/obj_file.cpp
@@ -0,0 +1,11 @@
+#include "obj_file.h"
+
+obj_file::obj_file()
+{
+
+}
+
+obj_file::~obj_file()
+{
+
+}
---
+++ source/obj_file.h
@@ -0,0 +1,10 @@
+#ifndef H_OBJ_FILE
+#define H_OBJ_FILE
+
+class obj_file
+{
+    obj_file();
+    ~obj_file();
+};
+
+#endif
