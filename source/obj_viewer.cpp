946cf204400123d7ad8a045ab5772fa75c08071f
Brandon <brandonreallen@gmail.com>
2018-02-19 18:24:56 -0500

    Add EBO

--- source/obj_viewer.cpp
+++ source/obj_viewer.cpp
@@ -49,13 +49,17 @@ main(int argc, char** argv)
     {
         glUseProgram(shader_program);
 
-        unsigned int VBO, VAO;
+        unsigned int VBO, VAO, EBO;
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
 
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(test_vertices), test_vertices, GL_STATIC_DRAW);
+
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(test_indices), test_indices, GL_STATIC_DRAW);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
@@ -66,7 +70,8 @@ main(int argc, char** argv)
             glClear(GL_COLOR_BUFFER_BIT);
 
             glBindVertexArray(VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
 
             glfwSwapBuffers(window);
 
--- source/obj_viewer.h
+++ source/obj_viewer.h
@@ -7,11 +7,16 @@
 #define OBJ_VIEWER_VIEWPORT_WIDTH OBJ_VIEWER_WINDOW_WIDTH
 #define OBJ_VIEWER_VIEWPORT_HEIGHT OBJ_VIEWER_WINDOW_HEIGHT
 
-
 float test_vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+    0.5f,  0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
-    0.0f,  0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f 
+};
+
+unsigned int test_indices[] = {  // note that we start from 0!
+    0, 1, 3,
+    1, 2, 3
 };
 
 #endif
